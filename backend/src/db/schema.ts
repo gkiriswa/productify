080d545106a104e7800cfc378e8fa206c3486638
gkiriswa <gkiriswa@gmail.com>
2026-01-13 09:50:12 +0300

    fix: correct import for Drizzle ORM and fix typo in updatedAt field

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -22,7 +22,7 @@ export const products = pgTable("products", {
         .notNull()
         .references(() => users.id, { onDelete: "cascade" }),
     createdAt: timestamp("created_at", { mode: "date" }).defaultNow().notNull(),
-    updadedAt: timestamp("updated_at", { mode: "date" }).defaultNow().notNull(),
+    updatedAt: timestamp("updated_at", { mode: "date" }).defaultNow().notNull(),
 });
 
 
