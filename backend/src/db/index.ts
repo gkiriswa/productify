d3451f278e639b74167abf1bf952545f05050119
gkiriswa <gkiriswa@gmail.com>
2026-01-13 09:18:27 +0300

    set up Drizzle ORM with PostgreSQL, configure environment variables, and implement basic API structure

---
+++ backend/src/db/index.ts
@@ -0,0 +1,22 @@
+import { drizzle } from "drizzle-orm/singlestore/driver";
+import { Pool } from "pg";
+import * as schema from "./schema";
+import { ENV } from "../config/env";
+
+
+if (!ENV.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set in environment variables");
+}
+
+
+const pool = new Pool({connectionString: ENV.DATABASE_URL,});
+
+pool.on("connect", () => {
+    console.log("Database connected successfully ✅");    
+});
+
+pool.on("error", (err) => {
+    console.error( "🎇Database connection error:", err);    
+});
+
+export const db = drizzle({ client:pool , schema });
