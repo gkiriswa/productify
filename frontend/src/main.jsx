03acd26dba441eac2891db5b0fae0c57db17eb8b
gkiriswa <gkiriswa@gmail.com>
2026-01-14 18:55:17 +0300

    refactor: clean up imports in main.jsx by removing unused hooks

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from "react-router"
-import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider, } from '@tanstack/react-query'
+import {  QueryClient, QueryClientProvider, } from '@tanstack/react-query'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -24,5 +24,5 @@ createRoot(document.getElementById('root')).render(
        </QueryClientProvider>
       </BrowserRouter>
       </ClerkProvider>
-  </StrictMode>,
+  </StrictMode>
 )
